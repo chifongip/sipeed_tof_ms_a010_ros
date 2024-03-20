4a56fee4413b62a69b95673fbefbd3bbddf02cd6
chifongip <chifongip@github.com>
2024-03-20 17:58:13 +0800

    add voxel filter and radius outlier removal filter

--- ros1/src/pcl_filter.cpp
+++ ros1/src/pcl_filter.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/radius_outlier_removal.h>
 #include <sensor_msgs/PointCloud2.h>
 
 #include <string>
@@ -16,6 +18,8 @@ public:
     nh_.getParam("point_cloud_processing_node/sub_topic", sub_topic);
     nh_.getParam("point_cloud_processing_node/MeanK", MeanK);
     nh_.getParam("point_cloud_processing_node/StddevMulThresh", StddevMulThresh);
+    nh_.getParam("point_cloud_processing_node/RadiusSearch", RadiusSearch);
+    nh_.getParam("point_cloud_processing_node/MinNeighborsInRadius", MinNeighborsInRadius);
 
     // Create a subscriber for the PointCloud2 message
     sub_ = nh_.subscribe<sensor_msgs::PointCloud2>(sub_topic, 1, &PointCloudProcessor::pointCloudCallback, this);
@@ -30,17 +34,34 @@ public:
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*msg, *cloud);
 
+    std::vector<int> indices;
+    pcl::removeNaNFromPointCloud(*cloud, *cloud, indices);
+
+    // Perform voxel grid downsampling
+    pcl::VoxelGrid<pcl::PointXYZ> voxel;
+    voxel.setInputCloud(cloud);
+    voxel.setLeafSize(0.1f, 0.1f, 0.1f);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr downsampled_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    voxel.filter(*downsampled_cloud);
+
     // Perform outlier removal using StatisticalOutlierRemoval filter
     pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-    sor.setInputCloud(cloud);
+    sor.setInputCloud(downsampled_cloud);
     sor.setMeanK(MeanK);  // Number of neighbors to analyze for each point
     sor.setStddevMulThresh(StddevMulThresh);  // Standard deviation threshold
     pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     sor.filter(*filtered_cloud);
 
+    pcl::RadiusOutlierRemoval<pcl::PointXYZ> ror;
+    ror.setInputCloud(filtered_cloud);
+    ror.setRadiusSearch(RadiusSearch);  // Radius within which to search for neighbors
+    ror.setMinNeighborsInRadius(MinNeighborsInRadius);  // Minimum number of neighbors within the radius
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud_2(new pcl::PointCloud<pcl::PointXYZ>);
+    ror.filter(*filtered_cloud_2);
+
     // Convert filtered PointCloud back to PointCloud2 message
     sensor_msgs::PointCloud2 filtered_msg;
-    pcl::toROSMsg(*filtered_cloud, filtered_msg);
+    pcl::toROSMsg(*filtered_cloud_2, filtered_msg);
     filtered_msg.header = msg->header;
 
     // Publish the processed PointCloud2 message
@@ -54,6 +75,8 @@ private:
 
   int MeanK;
   double StddevMulThresh;
+  double RadiusSearch;
+  int MinNeighborsInRadius;
   string sub_topic;
 };
 
